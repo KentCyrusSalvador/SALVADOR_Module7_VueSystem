5b319edbabfb1c4b507ede9ee0f5329dea467fb7
Kent Cyrus Salvador <62212024@chcc.edu.ph>
2026-08-25 16:49:25 +0800

    Fix GitHub Pages base path routing in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {
-    environment: 'jsdom'
-  }
+  base: '/SALVADOR_Module7_VueSystem/',
 })
